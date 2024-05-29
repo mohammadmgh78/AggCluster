0275efaecd4fa70fe06243facd3a5749616875af
mohammadmgh78 <84505068+mohammadmgh78@users.noreply.github.com>
2024-05-29 01:20:56 -0500

    Update core.py

--- CallCenterSim/core.py
+++ CallCenterSim/core.py
@@ -14,12 +14,12 @@ shift_time_1 = 8*60
 shift_time_2 = 16*60
 print("success")
 def Exponential(lambd):
-    r = random.random()
+    r = np.random.random()
     return -(1 / lambd) * math.log(r)
 
 
 def Uniform(a, b):
-    r = random.random()
+    r = np.random.random()
     return a + (b - a) * r
 
 
