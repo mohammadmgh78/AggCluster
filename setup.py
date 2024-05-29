58c7ffcd505546ce570661d3d104c714356a492d
mohammadmgh78 <84505068+mohammadmgh78@users.noreply.github.com>
2024-05-29 00:22:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='CallCenterSim',
     version='0.1.0',
     packages=find_packages(),
     description='A simple example package',
@@ -12,6 +12,6 @@ setup(
     long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_package',
+    url='https://github.com/mohammadmgh78/CallCenterSim',
     license='MIT',
 )
