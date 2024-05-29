d930eb8ba15276d3bb118be1911c99f097159ed7
mohammadmgh78 <84505068+mohammadmgh78@users.noreply.github.com>
2024-05-29 01:38:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='CallCenterSim',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     description='A package for simulating call center operations',
     long_description=open('README.md').read(),
